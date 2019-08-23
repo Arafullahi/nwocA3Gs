b41f781fdc2ddf62bd0a1162f8b741464971830d
Araf <arafullata@gmail.com>
2019-08-23 07:48:34 +0530

    Service infinity chages

--- src/main/java/com/nwoc/a3gs/group/app/controller/ServiceController.java
+++ src/main/java/com/nwoc/a3gs/group/app/controller/ServiceController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.nwoc.a3gs.group.app.dto.ServicesDTO;
 import com.nwoc.a3gs.group.app.model.Services;
@@ -26,7 +27,7 @@ import com.nwoc.a3gs.group.app.services.ServicesService;
 
 import javassist.NotFoundException;
 @CrossOrigin(origins = "*", maxAge = 3600)
-@Controller
+@RestController
 @RequestMapping("/api")
 public class ServiceController {
 
